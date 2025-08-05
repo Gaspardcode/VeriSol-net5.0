2f41d5022b2143925677b241569abe4337890757
g_code <gaspard.torterat-slanda@epita.fr>
2025-08-05 15:31:05 +0200

    feat: improving some bash scripts

--- ERC20_benchmark.sh
+++ ERC20_benchmark.sh
@@ -2,6 +2,11 @@ for file in $(ls ERC20);
 do 
     #solv=$(cat ERC20/$file | grep -oP "pragma solidity \K[^;]+")
     #solc-select use $solv --always-install
-    ./verisol.sh ERC20/$file;
+    ./verisol.sh ERC20/$file
 
+done
+
+for file in $(ls *.bpl);
+do
+    /home/g_code/VeriSol-net5.0/Sources/VeriSol/bin/Debug/boogie -doModSetAnalysis -inline:spec  -inlineDepth:4 -proc:BoogieEntry_* $file
 done
