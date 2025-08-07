ae1a8eedb0c8b0dfc2c4a3757e38675eede2557c
g_code <gaspard.torterat-slanda@epita.fr>
2025-08-07 16:49:07 +0200

    feat: improved script

--- scripts/count_lines_of_cs.sh
+++ scripts/count_lines_of_cs.sh
@@ -1,5 +1,26 @@
-a=0
-for file in $(ls $1/*.cs ); do
- 	a=$(( $(cat $file | wc -l ) + $a ))
-done
-echo $a lines of code. Good luck buddy.
+if [ $# -eq 0 ]; then
+	echo 'Usage <dir> [-p for ratio]'
+	exit 1;
+fi
+
+echo $#
+
+if [ $# -eq 2 ]; then
+	ttl=0
+	for file in $(ls $1/*.cs ); do
+		cnt=$(cat $file | wc -l )
+		ttl=$(( $cnt + $ttl ))
+	done
+	for file in $(ls $1/*.cs ); do
+		cnt=$(cat $file | wc -l )
+		echo $file has ~$(( $cnt * 100 / $ttl))% of the code lines in this folder
+	done
+else
+	ttl=0
+	for file in $(ls $1/*.cs ); do
+		cnt=$(cat $file | wc -l )
+		echo $file has $cnt lines of code
+		ttl=$(( $cnt + $ttl ))
+	done
+fi
+echo Total : $ttl lines of code. Good luck buddy.
